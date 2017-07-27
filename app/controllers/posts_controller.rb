3153522915c28306d8db2eef8f2925374e5c3767
Corina <fluturelcorina@gmail.com>
2017-07-27 15:32:22 +0100

    Updating posts controller to accept user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_url
+    p post_params
+    @user = User.find(params[:user_id])
+    @post = @user.posts.create(post_params)
+    redirect_to user_path(@user)
   end
+
 
   def index
     @posts = Post.all
