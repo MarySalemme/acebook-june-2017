c8322460fa01246aed43b0a5e8bc7b07534e66d8
Corina <fluturelcorina@gmail.com>
2017-07-27 13:03:12 +0100

    Adding user-posts relationship

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.string :message
       t.timestamps
+      t.references :user, foreign_key: true
+
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725144335) do
+ActiveRecord::Schema.define(version: 20170727115435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
